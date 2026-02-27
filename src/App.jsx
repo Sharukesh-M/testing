a7ff58a263097a4d5207d6ca3742e5d7ddbe257b
Sharukesh-M <murugesansharukesh@gmail.com>
2026-02-27 15:28:51 +0530

    feat: Implement a new Harry Potter themed UI with a video loader, landing page, and core application structure, including new fonts and components.

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import RegisterPage from "./pages/RegisterPage";
 import ContactPage from "./pages/ContactPage";
 import AdminPage from "./pages/AdminPage";
 import SponsorsPage from "./pages/SponsorsPage";
+import HarryPotterChatbot from "./components/HarryPotterChatbot";
 
 import "./styles/global.css";
 
@@ -71,6 +72,7 @@ const AppContent = () => {
             </Routes>
           </main>
           <Footer />
+          <HarryPotterChatbot />
         </div>
       )}
     </>
